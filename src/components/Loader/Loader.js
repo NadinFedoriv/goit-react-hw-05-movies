d792f0d411812ceb8a8aedc1981ff6986e49798a
NadinFedoriv <nadinfedoriv@gmail.com>
2023-09-28 17:05:11 +0300

    change

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,13 +2,13 @@ import { MagnifyingGlass } from 'react-loader-spinner';
 
 export const Loader = () => (
   <MagnifyingGlass
-  visible={true}
-  height="80"
-  width="80"
-  ariaLabel="MagnifyingGlass-loading"
-  wrapperStyle={{}}
-  wrapperClass="MagnifyingGlass-wrapper"
-  glassColor = '#c0efff'
-  color = '#e15b64'
-/>
+    visible={true}
+    height="80"
+    width="80"
+    ariaLabel="MagnifyingGlass-loading"
+    wrapperStyle={{}}
+    wrapperClass="MagnifyingGlass-wrapper"
+    glassColor="#c0efff"
+    color="#4caf50"
+  />
 );
--- src/components/Reviews/Reviews.styled.js
+++ src/components/Reviews/Reviews.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const ReviewsWrapper = styled.div`
-  padding: 10px;  
+  padding: 10px;
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
 
@@ -15,17 +15,14 @@ export const ReviewsWrapper = styled.div`
     padding: 0;
     margin: 0;
 
-    li {
-      
-      h4 {
-        font-size: 18px;
-        margin-bottom: 10px;
-        color: #4caf50;
-      }
+    h4 {
+      font-size: 18px;
+      margin-bottom: 10px;
+      color: #4caf50;
+    }
 
-      p {
-        font-size: 16px;
-      }
+    p {
+      font-size: 16px;
     }
   }
 `;
